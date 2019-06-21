1893625adb1a910d4087e0310c2d623990c80e5f
Ashterix <doctor_ash_69@mail.ru>
2019-06-21 16:45:48 +0300

    introduction of multiple queries (butch request)

--- src/Ufo/JsonRpcBundle/Controller/ApiController.php
+++ src/Ufo/JsonRpcBundle/Controller/ApiController.php
@@ -5,6 +5,7 @@ namespace Ufo\JsonRpcBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Ufo\JsonRpcBundle\Exceptions\InvalidButchRequestExceptions;
 use Zend\Json\Server\Smd;
 
 class ApiController extends Controller
@@ -17,14 +18,43 @@ class ApiController extends Controller
     public function serverAction(Request $request)
     {
         $server = $this->get('ufo_api_server.zend_json_rpc_server_facade');
-
         if (Request::METHOD_GET == $request->getMethod()) {
             $smd = $server->getServiceMap();
             return new Response($smd, 200, ['Content-Type' => 'application/json']);
         }
         $server->getServer()->setReturnResponse(true);
-        return new Response($server->handle()->toJson(), 200, ['Content-Type' => 'application/json']);
 
+        try {
+            return $this->butchRequestAction($request);
+        } catch (InvalidButchRequestExceptions $e) {
+            return new Response($server->handle()->toJson(), 200, ['Content-Type' => 'application/json']);
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function butchRequestAction(Request $request)
+    {
+        $server = $this->get('ufo_api_server.zend_json_rpc_server_facade');
+
+        $raw = json_decode($request->getContent(), true);
+        if (false === isset($raw[0])
+            || false === is_array($raw[0])) {
+            throw new InvalidButchRequestExceptions();
+        }
+
+        $responses = [];
+        foreach ($raw as $options) {
+            $server->getServer()->clearRequestAndResponse();
+            $server->getServer()->getRequest()->setOptions($options);
+
+            $responses[] = $server->handle()->toJson();
+        }
+        $result = '[' . implode(', ', $responses) . ']';
+
+        return new Response($result, 200, ['Content-Type' => 'application/json']);
     }
 
     /**
---
+++ src/Ufo/JsonRpcBundle/Exceptions/InvalidButchRequestExceptions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: doctor
+ * Date: 21.06.19
+ * Time: 16:16
+ */
+
+namespace Ufo\JsonRpcBundle\Exceptions;
+
+
+class InvalidButchRequestExceptions extends \Exception
+{
+
+}
