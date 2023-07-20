c966bb4f75f7f0889deb6addc9d8e1bcc194dd26
Ashterix <ashterix69@gmail.com>
2023-07-20 09:54:58 +0300

    fix deprecated calls

--- src/UfoJsonRpcBundle.php
+++ src/UfoJsonRpcBundle.php
@@ -2,13 +2,8 @@
 
 namespace Ufo\JsonRpcBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class UfoJsonRpcBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-    }
 }
