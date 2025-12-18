bbf609b8cc72112e79835dfa139bf8d4fda20d10
Ashterix <ashterix69@gmail.com>
2025-12-18 10:11:33 +0200

    stub interface method

--- src/Server/RequestPrepare/RequestCarrier.php
+++ src/Server/RequestPrepare/RequestCarrier.php
@@ -6,16 +6,24 @@ use Ufo\RpcError\WrongWayException;
 use Ufo\RpcObject\IRpcSpecialParamHandler;
 use Ufo\RpcObject\RpcBatchRequest;
 use Ufo\RpcObject\RpcRequest;
+use Ufo\RpcObject\SpecialRpcParams;
+use Ufo\RpcObject\SpecialRpcParamsEnum;
 
 class RequestCarrier implements IRpcSpecialParamHandler
 {
     protected ?IRpcRequestCarrier $carrier = null;
-
-    public function __construct() {}
+    protected SpecialRpcParams $specialRpcParams;
+    protected array $specialParams = [];
 
     public function setCarrier(IRpcRequestCarrier $carrier): static
     {
         $this->carrier = $carrier;
+        try {
+            $params = $this->getRequestObject()->getSpecialParams();
+        } catch (WrongWayException) {
+            $params = [];
+        }
+        $this->specialRpcParams = SpecialRpcParamsEnum::fromArray($params);
         return $this;
     }
 
@@ -37,12 +45,8 @@ class RequestCarrier implements IRpcSpecialParamHandler
 
     public function getSpecialParams(): array
     {
-        try {
-            $params = $this->getRequestObject()->getSpecialParams();
-        } catch (WrongWayException) {
-            $params = [];
-        }
-        return $params;
+        $this->specialRpcParams = SpecialRpcParamsEnum::fromArray($this->specialParams);
+        return $this->specialRpcParams->toArray();
     }
 
     public function getCarrier(): ?IRpcRequestCarrier
@@ -50,4 +54,15 @@ class RequestCarrier implements IRpcSpecialParamHandler
         return $this->carrier;
     }
 
+    public function setParam(string $name, mixed $value): static
+    {
+        $this->specialParams[$name] = $value;
+        return $this;
+    }
+
+    public function resetParams(): static
+    {
+        $this->specialParams = [];
+        return $this;
+    }
 }
